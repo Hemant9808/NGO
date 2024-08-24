be6bda8cef5e27df5b942d3eac22127ceb9979cb
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-08-24 23:07:14 +0530

    fix

--- src/pages/Campaigns.tsx
+++ src/pages/Campaigns.tsx
@@ -3,6 +3,7 @@ import OurCampaigns from '../components/OurCampaigns.tsx';
 import OurCampaignsBlog from '../components/OurCampaignsBlog.tsx';
 
 function Campaigns() {
+  
   return (
     <div className="flex flex-col">
       <OurCampaigns />
