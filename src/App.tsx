2a1e7899e4246760a9c5ccf7b88cae4d83aeec65
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-09-10 20:02:10 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.tsx"; // Import the Layout component
 import Home from "./pages/Home.tsx";
-import Team from "./pages/Team.tsx";
+// import Team from "./pages/Team.tsx";
 import Campaigns from "./pages/Campaigns.tsx";
 import Contact from "./pages/Contact.tsx";
 import Donate from "./pages/Donate.tsx";
